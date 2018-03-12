64dcc3cc5898a232f0e60da686e742c53131e576
Jonjo McKay <jonjo@jonjomckay.com>
2018-03-12 17:18:52 +0000

    Fixed hardcoded namespace

--- updater.sh
+++ updater.sh
@@ -4,4 +4,4 @@ stdbuf -o0 curl -sSk -H "Authorization: Bearer $KUBE_TOKEN"   https://$KUBERNETE
 line=`echo $line | sed 's/\"//g'`
 export cm_name=`echo $line | cut -d : -f1`
 export version=`echo $line |cut -d : -f2`
-kubectl --namespace=$MY_POD_NAMESPACE get deployments -o json  |  jq  --unbuffered   '.items[]|select(.spec.template.spec.containers[0].envFrom[0].configMapRef.name == env.cm_name)|.metadata.name'| xargs -I {} kubectl --namespace=dev patch deployment {}  --type json  -p='[{"op": "replace", "path": "/spec/template/metadata/labels/version", "value":'"'$version'"'}]'  ;done
+kubectl --namespace=$MY_POD_NAMESPACE get deployments -o json  |  jq  --unbuffered   '.items[]|select(.spec.template.spec.containers[0].envFrom[0].configMapRef.name == env.cm_name)|.metadata.name'| xargs -I {} kubectl --namespace=$MY_POD_NAMESPACE patch deployment {}  --type json  -p='[{"op": "replace", "path": "/spec/template/metadata/labels/version", "value":'"'$version'"'}]'  ;done
